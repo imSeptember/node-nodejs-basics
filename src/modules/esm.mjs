2348fcf93dac6f6ca876c09f2ce5e4b3925e103a
Vladislav Kalenskiy <108691131+imSeptember@users.noreply.github.com>
2024-01-25 17:22:13 +0200

    1

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,41 +1,36 @@
 import path from 'path';
 import { release, version } from 'os';
 import { createServer } from 'http';
-import './files/c.js'; // Make sure the path is correct
+import './files/c.js';
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
-const aJson = require('./files/a.json');
-const bJson = require('./files/b.json');
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = aJson;
-    console.log(unknownObject);
+    unknownObject = require('./files/a.json');
 } else {
-    unknownObject = bJson;
-    console.log(unknownObject);
+    unknownObject = require('./files/b.json');
 }
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${path.sep}"`);
 
-// Getting the current file's path
 const currentFilePath = new URL(import.meta.url).pathname;
 console.log(`Path to current file is ${currentFilePath}`);
 
-// Getting the current directory's path
-const currentDirPath = path.dirname(currentFilePath);
-console.log(`Path to current directory is ${currentDirPath}`);
+console.log(`Path to current directory is ${path.dirname(currentFilePath)}`);
 
 const myServer = createServer((_, res) => {
     res.end('Request accepted');
 });
 
 const PORT = 3000;
+
+console.log(unknownObject);
 
 myServer.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
