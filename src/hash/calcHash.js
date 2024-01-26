0c9303cdfc6a2c3eb211052590878ad73183bdd2
Vladislav Kalenskiy <108691131+imSeptember@users.noreply.github.com>
2024-01-26 20:23:45 +0200

    123

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,45 @@
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import crypto from 'crypto';
+import { Readable } from 'stream';
+
+const currentFileUrl = import.meta.url;
+const __filename = fileURLToPath(currentFileUrl);
+const __dirname = path.dirname(__filename);
+
+const sourceFilePath = path.join(
+    __dirname,
+    'files',
+    'fileToCalculateHashFor.txt'
+);
+
 const calculateHash = async () => {
-    // Write your code here 
+    return new Promise((resolve, reject) => {
+        const readStream = fs.createReadStream(sourceFilePath);
+
+        const hashStream = crypto.createHash('sha256');
+
+        readStream.on('data', (chunk) => {
+            hashStream.update(chunk);
+        });
+
+        readStream.on('end', () => {
+            const hash = hashStream.digest('hex');
+
+            const hashStreamOutput = new Readable();
+            hashStreamOutput.push(hash);
+            hashStreamOutput.push(null); // Indicates the end of the stream
+            hashStreamOutput.pipe(process.stdout);
+
+            resolve();
+        });
+
+        readStream.on('error', (err) => {
+            console.error('Error reading the file:', err);
+            reject(err);
+        });
+    });
 };
 
 await calculateHash();
