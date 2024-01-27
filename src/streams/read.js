da8192b6daf086fefac6d3cdd192c28ad420b78d
Vladislav Kalenskiy <108691131+imSeptember@users.noreply.github.com>
2024-01-27 20:36:49 +0200

    done

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,26 @@
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const currentFileUrl = import.meta.url;
+const __filename = fileURLToPath(currentFileUrl);
+const __dirname = path.dirname(__filename);
+
+const sourceFilePath = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+    const readStream = fs.createReadStream(sourceFilePath);
+    //
+    readStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
+
+    readStream.on('end', () => {
+        process.stdout.end();
+    });
+
+    //OR JUST
+    // readStream.pipe(process.stdout);
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+'use strict';
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const currentFileUrl = import.meta.url;
+const __filename = fileURLToPath(currentFileUrl);
+const __dirname = path.dirname(__filename);
+const sourceFilePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+
 const write = async () => {
-    // Write your code here 
+    const writeStream = fs.createWriteStream(sourceFilePath);
+    process.stdin.pipe(writeStream);
 };
 
 await write();
