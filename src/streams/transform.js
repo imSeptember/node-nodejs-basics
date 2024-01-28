99eac0e898d5cf736f55e1cc67837027f5ae2c5e
Vladislav Kalenskiy <108691131+imSeptember@users.noreply.github.com>
2024-01-28 15:21:25 +0200

    done

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const readStream = process.stdin;
+    const writeStream = process.stdout;
+    console.log(readStream);
+
+    const reverse = new Transform({
+        transform(chunk, encoding, callback) {
+            callback(null, chunk.toString().split('').reverse().join(''));
+        },
+    });
+
+    readStream.pipe(reverse).pipe(writeStream);
 };
 
 await transform();
