4f484feb8855bce085725fea549887ece5c4bc8a
Vladislav Kalenskiy <108691131+imSeptember@users.noreply.github.com>
2024-01-29 16:04:57 +0200

    1

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import * as zlib from 'zlib';
+
+const currentFileUrl = import.meta.url;
+const __filename = fileURLToPath(currentFileUrl);
+const __dirname = path.dirname(__filename);
+
+const sourceFilePath = path.join(__dirname, 'files', 'fileToCompress.txt');
+const newFilePath = path.join(__dirname, 'files', 'archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+    const gzib = zlib.createGzip();
+    const readStream = fs.createReadStream(sourceFilePath);
+    const writeStream = fs.createWriteStream(newFilePath);
+    readStream.pipe(gzib).pipe(writeStream);
 };
 
 await compress();
